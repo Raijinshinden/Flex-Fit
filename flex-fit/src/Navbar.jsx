8ac1f525d464d077962b1de621ac9b0691ac4c87
Ryan Dempsey <rjd9896@gmail.com>
2024-04-13 23:55:14 +0900

    Update Navbar.jsx

--- flex-fit/src/Navbar.jsx
+++ flex-fit/src/Navbar.jsx
@@ -2,6 +2,17 @@ export default function Navbar() {
     return(
         <nav className="navbar">
             <a href="/" className="site-title">Flex Fit</a>
+            <ul>
+                <li>
+                    <a href="about">About</a>
+                </li>
+                <li>
+                    <a href="classes">Classes</a>
+                </li>
+                <li>
+                    <a href="classes">Classes</a>
+                </li>
+            </ul>
         </nav>
     )
 }
