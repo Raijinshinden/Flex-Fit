6be22b8863ff55a5634066cc42ee327a007f7e89
Ryan Dempsey <rjd9896@gmail.com>
2024-04-14 01:27:23 +0900

    edits

--- flex-fit/src/Home.jsx
+++ flex-fit/src/Home.jsx
@@ -2,6 +2,13 @@
 
 export default function Home(){
     return(
-        <section id="/"></section>
+        <section id="/">
+            <div className="hero-container">
+                <div className="hero-img-container">
+                    <img src=".assets/images/man-ropes.jpeg"></img>
+                    <p className="hero-text">Join our gym if you want to become  </p>
+                </div>
+            </div>
+        </section>
     )
 }
