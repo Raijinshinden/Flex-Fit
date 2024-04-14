6f9522ea88aadc2fb7f4c2f1d8489486b0dca442
Ryan Dempsey <rjd9896@gmail.com>
2024-04-14 22:57:46 +0900

    Update Home.jsx

--- flex-fit/src/Home.jsx
+++ flex-fit/src/Home.jsx
@@ -4,8 +4,8 @@ function Home(){
         <section id="heroSection" className="hero-section">
             <div className="hero-section-content-box">
                 <div className="hero-section-content">
-                    <p className="section-title">Join our gym if you want to feel</p>
-                    
+                    <p className="section-title">Are you looking to become</p>
+                    <h1 className="buzzword"></h1>
                 </div>
             </div>
         </section>
