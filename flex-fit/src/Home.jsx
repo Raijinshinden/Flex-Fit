3005e0f44a9c1ad967a7bdb2fffe2888c28146f6
Ryan Dempsey <rjd9896@gmail.com>
2024-04-14 23:46:04 +0900

    edits

--- flex-fit/src/Home.jsx
+++ flex-fit/src/Home.jsx
@@ -17,7 +17,7 @@ function Home(){
                 <button className="btn btn-primary">Contact Us</button>
             </div>
             <div className="hero-section-img">
-                <img src="/public/images/man-treadmill.jpg" alt="Hero Section"/>
+                <img src="/images/man-treadmill.jpg" alt="Hero Section"/>
             </div>
         </section>
     );
