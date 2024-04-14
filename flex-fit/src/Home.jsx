41b8ce8f12edd71bfbf8470098fd4378906a9be2
Ryan Dempsey <rjd9896@gmail.com>
2024-04-14 23:01:51 +0900

    Update Home.jsx

--- flex-fit/src/Home.jsx
+++ flex-fit/src/Home.jsx
@@ -1,13 +1,13 @@
 
+
+const buzzwords = ["stronger", "faster", "healthier", "confident", ""]
+
 function Home(){
     return (
         <section id="heroSection" className="hero-section">
             <div className="hero-section-content-box">
                 <div className="hero-section-content">
                     <p className="section-title">Are you looking to become</p>
-                    <h1 className="Hero-title">
-                        <span className="buzzWord"></span>
-                    </h1>
                 </div>
             </div>
         </section>
