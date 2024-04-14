2b2b77c6e99354f4676623351219c0d8b923dcaf
Ryan Dempsey <rjd9896@gmail.com>
2024-04-14 09:24:09 +0900

    Made some edits to the css

--- flex-fit/src/Home.jsx
+++ flex-fit/src/Home.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 
+import womanStretch from "./assets/images/gym/woman-stretch.webp";
+import womanBarbell from "./assets/images/gym/woman-barbell.webp";
+import manRopes from "./assets/images/gym/man-ropes.jpeg"
+
+const carouselImg = [womanStretch, womanBarbell, manRopes];
+
+
+
+function ImageSlider({imageUrls}){
+
+}
 
 export default function Home(){
     return(
-        <section id="/">
-            <div className="hero-container">
-                <div className="hero-img-container">
-                <p className="hero-text">Join our gym if you want to become  </p>
+            <section id="/">
+                <div className="hero-container">
+                    <div className="hero-img-container">
+                    <p className="hero-text">Join our gym if you want to become  </p>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
     )
 }
