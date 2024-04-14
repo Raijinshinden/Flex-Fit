bf797931fb173a78d5a39f5c58b8e33dff81b104
Ryan Dempsey <rjd9896@gmail.com>
2024-04-14 23:18:33 +0900

    Added H1 and P elements to the hero section

--- flex-fit/src/Home.jsx
+++ flex-fit/src/Home.jsx
@@ -1,13 +1,18 @@
 
 
-const buzzwords = ["stronger", "faster", "healthier", "confident", ""]
+
 
 function Home(){
     return (
         <section id="heroSection" className="hero-section">
             <div className="hero-section-content-box">
                 <div className="hero-section-content">
-                    <p className="section-title">Are you looking to become</p>
+                    <h1 className="section-title"><span>Are you looking to get in shape,</span>{" "} 
+                    <br/>
+                    but short on time?</h1>
+                    <p className="hero-description"><span>Flex Fit is a state-of-the-art 24hr gym.</span>{""}
+                    <br />
+                    So you can come and exercise whenever you want to.</p>
                 </div>
             </div>
         </section>
