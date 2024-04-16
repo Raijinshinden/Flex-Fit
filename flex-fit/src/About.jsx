771d0ce47748052e88e912975ef5304c22cda8fb
Ryan Dempsey <rjd9896@gmail.com>
2024-04-17 00:16:57 +0900

    edits

--- flex-fit/src/About.jsx
+++ flex-fit/src/About.jsx
@@ -2,6 +2,12 @@
 
 export default function About(){
     <section id="about-section">
-        <hr></hr>
+        <div className="about-section-content-box">
+            <div className="about-section-content">
+                <div className="about-description">
+                    <h1>About</h1>
+                </div>
+            </div>
+        </div>
     </section>
 }
