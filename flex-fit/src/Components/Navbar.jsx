2def8303816282f58070db4b97a348b289f19459
Ryan Dempsey <rjd9896@gmail.com>
2024-04-25 00:17:30 +0900

    Update Navbar.jsx

--- flex-fit/src/Components/Navbar.jsx
+++ flex-fit/src/Components/Navbar.jsx
@@ -1,21 +1,11 @@
-export default function Navbar() {
-    return(
-        <nav>
-            <h1 href="/">Flex-Fit</h1>
-            <ul className="flex ">
-                <li className="mr-6">
-                    <a href="/">Home</a>
-                </li>
-                <li className="mr-6">
-                    <a href="about">About</a>
-                </li>
-                <li className="mr-6">
-                    <a href="classes">Classes</a>
-                </li>
-                <li className="mr-6"> 
-                    <a href="contact">Contact Us</a>
-                </li>
-            </ul>
-        </nav>
-    )
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='color-[#ffffff]'>
+        Flex-Fit
+    </div>
+  )
 }
+
+export default Navbar
