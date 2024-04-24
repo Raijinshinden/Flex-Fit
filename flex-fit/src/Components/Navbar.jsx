62663c7b803d1e763104cfae02a980f6028b79a8
Ryan Dempsey <rjd9896@gmail.com>
2024-04-25 00:28:34 +0900

    Update Navbar.jsx

--- flex-fit/src/Components/Navbar.jsx
+++ flex-fit/src/Components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-'>
-        <div className='text-4xl font-bold'>
+    <nav className='bg-gray-800'>
+        <div className='text-4xl font-bold color-amber-500'>
             Flex-Fit
         </div>
     </nav>
