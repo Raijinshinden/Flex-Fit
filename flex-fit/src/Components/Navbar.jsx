d8acf9381bf94857ef7a8ae0032e88ba480e87ef
Ryan Dempsey <rjd9896@gmail.com>
2024-04-26 01:08:01 +0900

    edits

--- flex-fit/src/Components/Navbar.jsx
+++ flex-fit/src/Components/Navbar.jsx
@@ -2,25 +2,29 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex bg-gray-800  hover:cursor-pointer'>
-        <div className='text-4xl font-bold text-amber-500'>
-            Flex-Fit
-        </div>
-        <ul className='flex ml-2 text-amber-500 hover:cursor-pointer'>
-            <li>
-                Home
-            </li>
-            <li>
-                About
-            </li>
-            <li>
-                Classes
-            </li>
-            <li>
-                Contact
-            </li>
-        </ul>
-    </nav>
+    <header className='bg-gray-800'>
+        <nav className='flex justify-between items-center w-[92%]'>
+            <div className='text-5xl font-bold text-amber-500 hover:cursor-pointer'>
+                Flex-Fit
+            </div>
+            <div>
+                <ul className='flex items-center gap-[4vw]'>
+                    <li>
+                        <a className='text-xl ml- p-2 text-amber-500 hover:bg-white hover:text-amber-600 hover:border-solid hover:border-2 hover:border-amber-500' href='#'>Home</a>
+                    </li>
+                    <li>
+                        <a className='text-xl ml- p-2 text-amber-500 hover:bg-white hover:text-amber-600 hover:border-solid hover:border-2 hover:border-amber-500' href='#'>About</a>
+                    </li>
+                    <li>
+                        <a className='text-xl ml- p-2 text-amber-500 hover:bg-white hover:text-amber-600 hover:border-solid hover:border-2 hover:border-amber-500' href='#'>Classes</a>
+                    </li>
+                    <li>
+                        <a className='text-xl ml- p-2 text-amber-500 hover:bg-white hover:text-amber-600 hover:border-solid hover:border-2 hover:border-amber-500' href='#'>Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
   )
 }
 
