ff523ec880a7af6c3a9d3c119d4049f3f9ee65a4
Ryan Dempsey <rjd9896@gmail.com>
2024-04-26 01:16:25 +0900

    Update Navbar.jsx

--- flex-fit/src/Components/Navbar.jsx
+++ flex-fit/src/Components/Navbar.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <header className='bg-gray-800'>
-        <nav className='flex justify-between items-center w-[92%]'>
+    <header className='bg-gray-800 h-15'>
+        <nav className='flex justify-between items-center w-[92%] mx-auto py-2'>
             <div className='text-5xl font-bold text-amber-500 hover:cursor-pointer'>
                 Flex-Fit
             </div>
             <div>
-                <ul className='flex items-center gap-[4vw]'>
+                <ul className='flex items-center gap-[7vw]'>
                     <li>
-                        <a className='text-xl ml- p-2 text-amber-500 hover:bg-white hover:text-amber-600 hover:border-solid hover:border-2 hover:border-amber-500' href='#'>Home</a>
+                        <a className='border-rounded text-xl ml- p-2 text-amber-500 hover:bg-white hover:text-amber-600 hover:border-solid hover:border-2 hover:border-amber-500' href='#'>Home</a>
                     </li>
                     <li>
-                        <a className='text-xl ml- p-2 text-amber-500 hover:bg-white hover:text-amber-600 hover:border-solid hover:border-2 hover:border-amber-500' href='#'>About</a>
+                        <a className='border-rounded text-xl ml- p-2 text-amber-500 hover:bg-white hover:text-amber-600 hover:border-solid hover:border-2 hover:border-amber-500' href='#'>About</a>
                     </li>
                     <li>
-                        <a className='text-xl ml- p-2 text-amber-500 hover:bg-white hover:text-amber-600 hover:border-solid hover:border-2 hover:border-amber-500' href='#'>Classes</a>
+                        <a className='border-rounded text-xl ml- p-2 text-amber-500 hover:bg-white hover:text-amber-600 hover:border-solid hover:border-2 hover:border-amber-500' href='#'>Classes</a>
                     </li>
                     <li>
-                        <a className='text-xl ml- p-2 text-amber-500 hover:bg-white hover:text-amber-600 hover:border-solid hover:border-2 hover:border-amber-500' href='#'>Contact Us</a>
+                        <a className='border-rounded text-xl ml- p-2 text-amber-500 hover:bg-white hover:text-amber-600 hover:border-solid hover:border-2 hover:border-amber-500' href='#'>Contact Us</a>
                     </li>
                 </ul>
             </div>
