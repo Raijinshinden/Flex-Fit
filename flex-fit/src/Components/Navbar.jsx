3c29950379ac0eb5ac46559ed8b0f9b75bcedfc7
Ryan Dempsey <rjd9896@gmail.com>
2024-04-26 01:21:21 +0900

    Update Navbar.jsx

--- flex-fit/src/Components/Navbar.jsx
+++ flex-fit/src/Components/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
             <div className='text-5xl font-bold text-amber-500 hover:cursor-pointer'>
                 Flex-Fit
             </div>
-            <div>
-                <ul className='flex items-center gap-[7vw]'>
+            <div className='absolute bg-gray-800 min-h-[60vh] left-0 top-[9%]'>
+                <ul className='flex md:flex-row flex-col items-center gap-[7vw]'>
                     <li>
                         <a className='border-rounded text-xl ml- p-2 text-amber-500 hover:bg-white hover:text-amber-600 hover:border-solid hover:border-2 hover:border-amber-500' href='#'>Home</a>
                     </li>
