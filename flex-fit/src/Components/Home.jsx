40613f1070fc4d9baaa8b9b8ed5614d35db09a9f
Ryan Dempsey <rjd9896@gmail.com>
2024-04-29 00:01:35 +0900

    Update Home.jsx

--- flex-fit/src/Components/Home.jsx
+++ flex-fit/src/Components/Home.jsx
@@ -5,16 +5,14 @@
 function Home(){
     return (
         <section id="heroSection" className="hero-section">
-            <img src="/images/man-treadmill.jpg" className="w-full h-screen object-cover"/>
             <div className="hero-section-content-box">
-                <div className="hero-section-content">
+                <img src="/images/man-treadmill.jpg" className="w-full h-screen object-cover"/>
                     <h1 className="text-4xl font-bold text-amber-500"><span>Are you looking to get in shape,</span>{" "} 
                     <br/>
                     but short on time?</h1>
                     <p className="text-xl"><span>Flex-Fit is a state-of-the-art 24hr gym.</span>{""}
                     <br />
                     So you can come and exercise whenever you want to.</p>
-                </div>
                 <button className="bg-amber-500 px-2 py-2 text-white rounded-full">Contact Us</button>
             </div>
         </section>
