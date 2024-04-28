d5d23a8890deb9f0bdb0c599f6d856cd7508621c
Ryan Dempsey <rjd9896@gmail.com>
2024-04-28 23:49:11 +0900

    Update About.jsx

--- flex-fit/src/Components/About.jsx
+++ flex-fit/src/Components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <>
-    <div className='font-bold text-size-12'>About Us</div>
+    <div className='font-bold text-4'>About Us</div>
     <p>
         Flex-Fit was founded with the idea that people should be able to exercise whenever they want to.
     </p>
