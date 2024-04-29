8e9048ad2cc7995c194b42a769aa27158482b2c8
Ryan Dempsey <rjd9896@gmail.com>
2024-04-29 23:39:54 +0900

    Update Navbar.jsx

--- flex-fit/src/Components/Navbar.jsx
+++ flex-fit/src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {AiOutlineMenu} from 'react-icons/ai'
+import {AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlineMail} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
