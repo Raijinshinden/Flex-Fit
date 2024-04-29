4dd548909d4307fffcb9859a892bb53e35a2d0f7
Ryan Dempsey <rjd9896@gmail.com>
2024-04-30 00:15:02 +0900

    Update Navbar.jsx

--- flex-fit/src/Components/Navbar.jsx
+++ flex-fit/src/Components/Navbar.jsx
@@ -30,25 +30,6 @@ const Navbar = () => {
                 <button className='bg-amber-500 text-white px-5 py-2 mx-5 rounded-full hover:bg-white hover:text-amber-500 max-sm:hidden'>Contact Us</button>
                 <AiOutlineMenu className='absolute top-6 right-5 z-[99] fill-amber-500 h-4 w-4 hover:cursor-pointer md:hidden'/>
             </div>
-            <div className='md:block hidden fixed top-[25%] z-10'>
-                <div className='flex flex-col'>
-                    <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <AiOutlineHome size={20}/>
-                    </a>
-                    <a href="#work" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <GrProjects size={20}/>
-                    </a>
-                    <a href="#projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <AiOutlineProject size={20}/>
-                    </a>
-                    <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <BsPerson size={20}/>
-                    </a>
-                    <a href="#contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <AiOutlineMail size={20}/>
-                    </a>
-                </div>
-            </div>
         </nav>
     </header>
   )
