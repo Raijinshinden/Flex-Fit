9dc86d10630245d9424457fcc3162263bb758439
Ryan Dempsey <rjd9896@gmail.com>
2024-04-30 00:10:30 +0900

    Update Navbar.jsx

--- flex-fit/src/Components/Navbar.jsx
+++ flex-fit/src/Components/Navbar.jsx
@@ -25,8 +25,8 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className='flex items-center gap-6'>
-                <button className='bg-amber-500 text-white px-5 py-2 mx-5 rounded-full hover:bg-white hover:text-amber-500'>Contact Us</button>
-                <AiOutlineMenu className='absolute top-6 right-5 z-[99] hover:cursor-pointer bg-white md:hidden'/>
+                <button className='bg-amber-500 text-white px-5 py-2 mx-5 rounded-full hover:bg-white hover:text-amber-500 max-sm:hidden'>Contact Us</button>
+                <AiOutlineMenu className='absolute top-6 right-5 z-[99] fill-amber-500 h-4 w-4 hover:cursor-pointer md:hidden'/>
             </div>
         </nav>
     </header>
