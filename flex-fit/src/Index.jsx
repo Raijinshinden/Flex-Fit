3c7bd8138d616af671919f1be0ab154f346246d5
Ryan Dempsey <rjd9896@gmail.com>
2024-04-30 00:29:35 +0900

    edits

--- flex-fit/src/Index.jsx
+++ flex-fit/src/Index.jsx
@@ -2,11 +2,13 @@ import Navbar from "./Components/Navbar.jsx";
 import Home from "./Components/Home.jsx"
 import About from "./Components/About.jsx";
 import "./App.css";
+import Sidenav from "./Components/Sidenav.jsx";
 
 function App() {
   return(
       <>
       <Navbar />
+      <Sidenav />
       <Home />
       <About />
       </>
