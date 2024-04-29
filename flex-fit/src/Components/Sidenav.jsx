9e7b1bfda1f11e3a4a8ebba36e8d3dac53212921
Ryan Dempsey <rjd9896@gmail.com>
2024-04-30 00:36:22 +0900

    Update Sidenav.jsx

--- flex-fit/src/Components/Sidenav.jsx
+++ flex-fit/src/Components/Sidenav.jsx
@@ -15,7 +15,7 @@ function Sidenav() {
         <AiOutlineMenu onClick={handleNav} className='absolute top-6 right-5 z-[99] fill-amber-500 h-4 w-4 hover:cursor-pointer hover:bg-white hover:rounded md:hidden'/>
         {
             nav ? (
-                <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
+                <div className='w-full h-screen bg-gray-800/90 flex flex-col justify-center items-center z-20'>
                     <a onClick={handleNav} 
                       href="#main" 
                       className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
