258bf4e4ae2d58e9acc1d9191a993998fe330142
Ryan Dempsey <rjd9896@gmail.com>
2024-04-30 00:34:37 +0900

    edits

--- flex-fit/src/Components/Sidenav.jsx
+++ flex-fit/src/Components/Sidenav.jsx
@@ -12,7 +12,7 @@ function Sidenav() {
 
   return (
     <div>
-        <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+        <AiOutlineMenu onClick={handleNav} className='absolute top-6 right-5 z-[99] fill-amber-500 h-4 w-4 hover:cursor-pointer hover:bg-white hover:rounded md:hidden'/>
         {
             nav ? (
                 <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
@@ -56,29 +56,7 @@ function Sidenav() {
             : (
                 ''
             )}
-            <div className='md:block hidden fixed top-[25%] z-10'>
-                <div className='flex flex-col'>
-                    <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <AiOutlineHome size={20}/>
-                    </a>
-                    <a href="#work" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <GrProjects size={20}/>
-                    </a>
-                    <a href="#projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <AiOutlineProject size={20}/>
-                    </a>
-                    <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <BsPerson size={20}/>
-                    </a>
-                    <a href="#contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <AiOutlineMail size={20}/>
-                    </a>
-                </div>
             </div>
-
-
-
-    </div>
   );
 };
 
