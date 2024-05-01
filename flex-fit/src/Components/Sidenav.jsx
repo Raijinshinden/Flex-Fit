a9c4d35f03e9e20d4ad993506e1a4493eb49acce
Ryan Dempsey <rjd9896@gmail.com>
2024-05-01 23:45:34 +0900

    Update Sidenav.jsx

--- flex-fit/src/Components/Sidenav.jsx
+++ flex-fit/src/Components/Sidenav.jsx
@@ -18,38 +18,38 @@ function Sidenav() {
                 <div className='absolute w-full h-screen bg-gray-800/90 flex flex-col justify-center items-center z-20'>
                     <a onClick={handleNav} 
                       href="#main" 
-                      className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+                      className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-amber-500 shadow-amber-900 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                     >
-                      <AiOutlineHome size={20} />
-                      <span className='pl-4'>Home</span>
+                      <AiOutlineHome size={30} className='fill-white' />
+                      <span className='pl-4 text-white font-bold'>Home</span>
                     </a>
                     <a onClick={handleNav}  
                       href="#work" 
-                      className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+                      className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-amber-500 shadow-amber-900 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                     >
-                      <GrProjects size={20} />
-                      <span className='pl-4'>Work</span>
+                      <GrProjects size={30} className='fill-white' />
+                      <span className='pl-4 text-white font-bold'>About</span>
                     </a>
                     <a onClick={handleNav} 
                       href="#projects" 
-                      className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+                      className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-amber-500 shadow-amber-900 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                     >
-                      <AiOutlineProject size={20} />
-                      <span className='pl-4'>Projects</span>
+                      <AiOutlineProject size={30} className='fill-white' />
+                      <span className='pl-4 text-white font-bold'>Classes</span>
                     </a>
                     <a onClick={handleNav} 
                       href="#main" 
-                      className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+                      className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-amber-500 shadow-amber-900 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                     >
-                      <BsPerson size={20} />
-                      <span className='pl-4'>Resume</span>
+                      <BsPerson size={30} className='fill-white' />
+                      <span className='pl-4 text-white font-bold'>Trial</span>
                     </a>
                     <a onClick={handleNav} 
                       href="#contact" 
-                      className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+                      className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-amber-500 shadow-amber-900 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                     >
-                      <AiOutlineMail size={20} />
-                      <span className='pl-4'>Contact</span>
+                      <AiOutlineMail size={30} className='fill-white' />
+                      <span className='pl-4 text-white font-bold'>Contact</span>
                     </a>
                 </div>
             )
