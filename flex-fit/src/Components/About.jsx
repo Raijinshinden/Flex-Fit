3b31f1751bca83625d25ced8d76ab916132285bb
Ryan Dempsey <rjd9896@gmail.com>
2024-05-02 00:19:31 +0900

    Update About.jsx

--- flex-fit/src/Components/About.jsx
+++ flex-fit/src/Components/About.jsx
@@ -5,8 +5,8 @@ const About = () => {
     <>
     <div className='w-full h-6 bg-amber-500' />
     <div className='bg-gray-800'>
-      <div className='text-center font-bold text-3xl bg-gray-700'>
-        <h2 className='mt-2 rounded-full  text-amber-500 py-4'>About Us</h2>
+      <div className='rounded-full bg-gray-700'>
+        <h2 className='text-center font-bold text-3xl text-amber-500 py-4'>About Us</h2>
       </div>
       <p className='block text-xl'>
           Flex-Fit was founded with the idea that people should be able to exercise whenever they want to.
