35d25b975228cd9a72b36f695fd0983295c2e6ed
Ryan Dempsey <rjd9896@gmail.com>
2024-05-01 23:53:38 +0900

    Update About.jsx

--- flex-fit/src/Components/About.jsx
+++ flex-fit/src/Components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <>
-    <div className='w-full ' />
+    <div className='w-full h-6 bg-amber-500' />
     <div className='bg-gray-800'>
       <div className='text-center font-bold text-3xl text-amber-500 py-2'>About Us</div>
       <p className='text-xl'>
