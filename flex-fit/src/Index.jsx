9056ba7d7ad818bfc095725535f4fc3f056dd2a2
Ryan Dempsey <rjd9896@gmail.com>
2024-05-03 22:31:10 +0900

    Update Index.jsx

--- flex-fit/src/Index.jsx
+++ flex-fit/src/Index.jsx
@@ -1,9 +1,9 @@
 import Navbar from "./Components/Navbar.jsx";
 import Home from "./Components/Home.jsx"
 import About from "./Components/About.jsx";
-import "./App.css";
 import Sidenav from "./Components/Sidenav.jsx";
 import Contact from "./Components/Contact.jsx";
+import "./App.css";
 
 function App() {
   return(
