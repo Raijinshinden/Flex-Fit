0fa0c8d15a6f53c4e8b19f8144e02e341ec37655
Ryan Dempsey <rjd9896@gmail.com>
2024-05-03 22:33:55 +0900

    Update Contact.jsx

--- flex-fit/src/Components/Contact.jsx
+++ flex-fit/src/Components/Contact.jsx
@@ -13,14 +13,14 @@ const Contact = () => {
         >
             <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                 <div className='flex flex-col'>
-                    <label className='uppercase text-sm py-2'>Name</label>
+                    <label className='uppercase text-sm py-2 text-amber-500'>Name</label>
                     <input 
                     className='border-2 rounded-lg p-3 flex border-gray-300' 
                     type='text' 
                     name='name' />
                 </div>
                 <div className='flex flex-col'>
-                    <label className='uppercase text-sm py-2'>Phone</label>
+                    <label className='uppercase text-sm py-2 text-amber-500'>Phone</label>
                     <input 
                     className='border-2 rounded-lg p-3 flex border-gray-300' 
                     type="text" 
@@ -28,21 +28,21 @@ const Contact = () => {
                 </div>
             </div>
             <div className='flex flex-col py-2'>
-                <label className='uppercase text-sm py-2'>Email</label>
+                <label className='uppercase text-sm py-2 text-amber-500'>Email</label>
                 <input 
                 className='border-2 rounded-lg p-3 flex border-gray-300' 
                 type='email' 
                 name='name' />    
             </div>
             <div className='flex flex-col py-2'>
-                <label className='uppercase text-sm py-2'>Subject</label>
+                <label className='uppercase text-sm py-2 text-amber-500'>Subject</label>
                 <input 
                 className='border-2 rounded-lg p-3 flex border-gray-300' 
                 type='text' 
                 name='subject' />    
             </div>
             <div className='flex flex-col py-2'>
-                <label className='uppercase text-sm py-2'>Message</label>
+                <label className='uppercase text-sm py-2 text-amber-500'>Message</label>
                 <textarea 
                 className='border-2 rounded-lg p-3 border-gray-300' 
                 rows='10' 
