5170b891cadbc36ad39d1990ef8eae8d8a503053
Ryan Dempsey <rjd9896@gmail.com>
2024-05-03 22:58:06 +0900

    edit

---
+++ flex-fit/src/Components/Classes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Classes = () => {
+  return (
+    <div className='bg-gray-800'>
+        <h1 >
+            Classes
+        </h1>
+    </div>
+  )
+}
+
+export default Classes
--- flex-fit/src/Index.jsx
+++ flex-fit/src/Index.jsx
@@ -2,6 +2,7 @@ import Navbar from "./Components/Navbar.jsx";
 import Home from "./Components/Home.jsx"
 import About from "./Components/About.jsx";
 import Sidenav from "./Components/Sidenav.jsx";
+import Classes from "./Components/Classes.jsx";
 import Contact from "./Components/Contact.jsx";
 import SectionDivide from "./Components/SectionDivide.jsx";
 import "./App.css";
@@ -14,6 +15,8 @@ function App() {
       <Home />
       <SectionDivide />
       <About />
+      <SectionDivide />
+      <Classes />
       <SectionDivide />
       <Contact />
       </>
