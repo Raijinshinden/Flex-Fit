485b47f1d3720745a85689735fb19e41f801bc05
Ryan Dempsey <rjd9896@gmail.com>
2024-05-03 23:04:06 +0900

    Update Classes.jsx

--- flex-fit/src/Components/Classes.jsx
+++ flex-fit/src/Components/Classes.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Classes = () => {
   return (
     <div className='bg-gray-800'>
-        <h2 className=' bg-gray-700 text-center rounded-lg text-3xl text-amber-500'>
+        <h2 className=' bg-gray-700 text-center rounded-lg text-3xl font-bold text-amber-500 py-4'>
             Classes
         </h2>
     </div>
