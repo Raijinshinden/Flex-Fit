fa8e4efd536368c9fbbcb6d9aef66086a8fd6029
Ryan Dempsey <rjd9896@gmail.com>
2024-05-05 23:50:31 +0900

    Update Trial.jsx

--- flex-fit/src/Components/Trial.jsx
+++ flex-fit/src/Components/Trial.jsx
@@ -4,7 +4,7 @@ const Trial = () => {
   return (
     <div className='grid w-full m-auto md:pl-20 p-4 py-16 bg-gray-800'>
         <h2 className='mb-3 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
-            Classes
+            Trial
         </h2>
     </div>
   )
