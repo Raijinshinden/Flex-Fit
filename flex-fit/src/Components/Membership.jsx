69ab6d0bd0635ed599acef3c3c3c241753a991d4
Ryan Dempsey <rjd9896@gmail.com>
2024-05-05 23:54:44 +0900

    edit

--- flex-fit/src/Components/Membership.jsx
+++ flex-fit/src/Components/Membership.jsx
@@ -4,13 +4,13 @@ const Trial = () => {
   return (
     <div className='grid w-full m-auto md:pl-20 p-4 py-16 bg-gray-800'>
         <h2 className='mb-3 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
-            Trial
+            Membership
         </h2>
         <div>
             <div>
                 <img />
                 <h3>
-                    
+
                 </h3>
             </div>
         </div>
