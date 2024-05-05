c00ad3cf8e3a00deeb9d1c7cc2aa4272adcdc12b
Ryan Dempsey <rjd9896@gmail.com>
2024-05-05 23:49:38 +0900

    Update Trial.jsx

--- flex-fit/src/Components/Trial.jsx
+++ flex-fit/src/Components/Trial.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Trial = () => {
   return (
-    <div>Trial</div>
+    <div>
+        <h2 className='mb-3 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
+            Classes
+        </h2>
+    </div>
   )
 }
 
