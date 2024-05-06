f73cb633a35e0e5f8e3163792afe38cc73027b4a
Ryan Dempsey <rjd9896@gmail.com>
2024-05-06 23:47:06 +0900

    Update Classes.jsx

--- flex-fit/src/Components/Classes.jsx
+++ flex-fit/src/Components/Classes.jsx
@@ -9,11 +9,11 @@ const Classes = () => {
         <div className='flex justify-evenly gap-x-2 md:block'>
           <div className='bg-gray-700 rounded-xl px-2'>
             <img />
-            <h3 className='text-white text-2xl font-bold my-2'>
+            <h3 className='text-white text-2xl font-bold my-2 px-3'>
               Aerobics
             </h3>
           </div>
-          <div className='bg-gray-700 rounded-xl px-2'>
+          <div className='bg-gray-700 rounded-xl'>
             <img />
             <h3 className='text-white text-2xl font-bold my-2'>
               Boxing
