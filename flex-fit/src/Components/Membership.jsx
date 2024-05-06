c899d1b2b9296b221b9834ce5cca340a7c40e9ce
Ryan Dempsey <rjd9896@gmail.com>
2024-05-07 00:11:56 +0900

    Update Membership.jsx

--- flex-fit/src/Components/Membership.jsx
+++ flex-fit/src/Components/Membership.jsx
@@ -6,21 +6,21 @@ const Membership = () => {
         <h2 className='mb-3 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
             Membership
         </h2>
-        <div className='flex justify-evenly gap-5 sm:block'>
+        <div className='flex justify-evenly gap-5'>
             <div className='bg-gray-700 rounded-xl'>
                 <img />
-                <h3 className='text-white text-2xl text-center font-bold my-2 px-2'>
+                <h3 className='text-white text-2xl text-center font-bold my-2'>
                   Basic
                 </h3>
                 <p className='text-center text-white'>
                     This is our most basic package.  
                 </p>
-                <ul className='my-2 text-center text-wrap text-white list-inside list-disc'>
+                <ul className='my-2 text-center text-white list-inside list-disc'>
                   <li >
                     24/7 access to gym.
                   </li>
                   <li >
-                    You can take one trial class 
+                    You can take one trial class<br/> 
                     for free every month.
                   </li>
                 </ul>
@@ -28,7 +28,7 @@ const Membership = () => {
             </div>
             <div className='bg-gray-700 rounded-xl'>
                 <img />
-                <h3 className='text-white text-2xl text-center font-bold my-2 px-2'>
+                <h3 className='text-white text-2xl text-center font-bold my-2'>
                   Enthusiast
                 </h3>
                 <p className='text-center text-white'>
@@ -41,7 +41,7 @@ const Membership = () => {
             </div>
             <div className='bg-gray-700 rounded-xl'>
                 <img />
-                <h3 className='text-white text-2xl text-center font-bold my-2 px-2'>
+                <h3 className='text-white text-2xl text-center font-bold my-2'>
                   Gym Rat
                 </h3>
                 <p className='text-center text-white'>
