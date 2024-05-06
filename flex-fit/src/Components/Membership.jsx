c714d463fd29de64ba521523645d72f311fef6e3
Ryan Dempsey <rjd9896@gmail.com>
2024-05-06 17:09:22 +0900

    Update Membership.jsx

--- flex-fit/src/Components/Membership.jsx
+++ flex-fit/src/Components/Membership.jsx
@@ -6,39 +6,42 @@ const Membership = () => {
         <h2 className='mb-3 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
             Membership
         </h2>
-        <div className='flex justify-evenly md:block'>
+        <div className='flex justify-evenly gap-5 md:block'>
             <div className='bg-gray-700 rounded-xl'>
                 <img />
                 <h3 className='text-white text-2xl text-center font-bold my-2 px-2'>
                   Basic
                 </h3>
-                <p className='text-center'>
+                <p className='text-center text-white'>
                     This is our most basic package.<br/>
                     It allows members full access to <br/>
                     our facilities 24/7.
                 </p>
+                <button></button>
             </div>
             <div className='bg-gray-700 rounded-xl'>
                 <img />
-                <h3 className='text-white text-2xl font-bold my-2 px-2'>
+                <h3 className='text-white text-2xl text-center font-bold my-2 px-2'>
                   Enthusiast
                 </h3>
-                <p className='text-center'>
+                <p className='text-center text-white'>
                     This is our most basic package.<br/>
                     It allows members full access to <br/>
                     our facilities 24/7.
                 </p>
+                <button></button>
             </div>
             <div className='bg-gray-700 rounded-xl'>
                 <img />
-                <h3 className='text-white text-2xl font-bold my-2 px-2'>
+                <h3 className='text-white text-2xl text-center font-bold my-2 px-2'>
                   Gym Rat
                 </h3>
-                <p className='text-center'>
+                <p className='text-center text-white'>
                     This is our most basic package.<br/>
                     It allows members full access to <br/>
                     our facilities 24/7.
                 </p>
+                <button></button>
             </div>
         </div>
     </div>
