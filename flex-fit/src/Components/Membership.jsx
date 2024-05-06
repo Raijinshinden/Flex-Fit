3077fe2d1cbc329da2be535541e817db4e873c33
Ryan Dempsey <rjd9896@gmail.com>
2024-05-06 16:50:18 +0900

    Update Membership.jsx

--- flex-fit/src/Components/Membership.jsx
+++ flex-fit/src/Components/Membership.jsx
@@ -6,15 +6,13 @@ const Membership = () => {
         <h2 className='mb-3 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
             Membership
         </h2>
-        <div className='md:block'>
+        <div className='flex justify-evenly md:block'>
             <div>
                 <img />
                 <h3 className='text-white'>
                   Basic
                 </h3>
             </div>
-        </div>
-        <div>
             <div>
                 <img />
                 <h3 className='text-white'>
@@ -22,13 +20,11 @@ const Membership = () => {
                 </h3>
             </div>
             <div>
-            <div>
                 <img />
                 <h3 className='text-white'>
                   Gym Rat
                 </h3>
             </div>
-        </div>
         </div>
     </div>
   )
