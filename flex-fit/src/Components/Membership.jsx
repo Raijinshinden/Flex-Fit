c5177af4689b67c8aab0b477f2cd9ea855e3ea6d
Ryan Dempsey <rjd9896@gmail.com>
2024-05-07 23:54:45 +0900

    Update Membership.jsx

--- flex-fit/src/Components/Membership.jsx
+++ flex-fit/src/Components/Membership.jsx
@@ -46,7 +46,7 @@ const Membership = () => {
                     for free every month.
                   </li>
                   <li>
-                    Free ac
+                    Free acc
                   </li>
                 </ul>
                 <button></button>
