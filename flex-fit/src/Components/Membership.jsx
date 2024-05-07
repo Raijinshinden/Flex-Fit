6019707140451d4a30fdc50fbff3f1b6ce4b5c35
Ryan Dempsey <rjd9896@gmail.com>
2024-05-07 23:49:05 +0900

    Update Membership.jsx

--- flex-fit/src/Components/Membership.jsx
+++ flex-fit/src/Components/Membership.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Membership = () => {
   return (
     <div className='grid w-full m-auto md:pl-20 p-4 py-16 bg-gray-800'>
-        <h2 className='mb-3 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
+        <h2 className='mb-10 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
             Membership
         </h2>
         <div className='flex justify-evenly gap-5'>
