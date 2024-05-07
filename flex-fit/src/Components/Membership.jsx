3a9c0814ec653fdd823e5c533a147a9f52f77390
Ryan Dempsey <rjd9896@gmail.com>
2024-05-07 23:56:28 +0900

    Update Membership.jsx

--- flex-fit/src/Components/Membership.jsx
+++ flex-fit/src/Components/Membership.jsx
@@ -46,7 +46,7 @@ const Membership = () => {
                     for free every month.
                   </li>
                   <li>
-                    Free access to our paid car
+                    Free access to our paid carpark.
                   </li>
                 </ul>
                 <button></button>
