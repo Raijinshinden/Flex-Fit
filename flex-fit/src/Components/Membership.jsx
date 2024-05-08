09b16d74344d5740584e1f20a940bc41c1f0aa1b
Ryan Dempsey <rjd9896@gmail.com>
2024-05-08 23:55:33 +0900

    Update Membership.jsx

--- flex-fit/src/Components/Membership.jsx
+++ flex-fit/src/Components/Membership.jsx
@@ -25,7 +25,7 @@ const Membership = () => {
                     for free every month.
                   </li>
                 </ul>
-                <div className='text-center text-whi'>
+                <div className='text-center text-whit'>
                   $50.00
                 </div>
                 <button></button>
