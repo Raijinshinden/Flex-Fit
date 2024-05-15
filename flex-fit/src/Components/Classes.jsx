5b8a34ec1516c20bc1595bc2312986312a7d0232
Ryan Dempsey <rjd9896@gmail.com>
2024-05-15 23:53:01 +0900

    aerobics pic

--- flex-fit/src/Components/Classes.jsx
+++ flex-fit/src/Components/Classes.jsx
@@ -8,7 +8,7 @@ const Classes = () => {
         </h2>
         <div className='sm:col-start-1 row-start-2 mx-3 md:flex justify-evenly gap-x-2'>
           <div className='shadow-2xl hover:shadow-black hover:cursor-pointer sm:col-start-1 row-start-2 bg-gray-700 rounded-xl'>
-            <img />
+            <img src='public/images/aerobics.jpg'/>
             <h3 className='text-white text-2xl font-bold my-2 px-3'>
               Aerobics
             </h3>
