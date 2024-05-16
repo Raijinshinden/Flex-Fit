fd7e01f135657e5a15ef54d9a9317c6a3faf5e43
Ryan Dempsey <rjd9896@gmail.com>
2024-05-16 23:36:59 +0900

    Update Classes.jsx

--- flex-fit/src/Components/Classes.jsx
+++ flex-fit/src/Components/Classes.jsx
@@ -6,7 +6,7 @@ const Classes = () => {
         <h2 className='sm:col-start-1 row-start-1 mb-3 mx-3 md:mb-3 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
             Classes
         </h2>
-        <div className='sm:col-start-1 row-start-2 mx-3 md:flex justify-evenly gap-x-2'>
+        <div className='sm:col-start-1 row-start-2 mx-3 mb-2 md:flex justify-evenly gap-x-2'>
           <div className='shadow-2xl hover:shadow-black hover:cursor-pointer sm:col-start-1 row-start-2 bg-gray-700 rounded-xl max-w-md:'>
             <img src='/images/aerobics.jpg' className='p-2'/>
             <h3 className='text-white text-2xl text-center font-bold my-2 px-3'>
