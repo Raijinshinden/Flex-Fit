445690af94e96b84ba8874e385eb4ce4cebdb872
Ryan Dempsey <rjd9896@gmail.com>
2024-05-16 23:38:08 +0900

    Update Classes.jsx

--- flex-fit/src/Components/Classes.jsx
+++ flex-fit/src/Components/Classes.jsx
@@ -6,8 +6,8 @@ const Classes = () => {
         <h2 className='sm:col-start-1 row-start-1 mb-3 mx-3 md:mb-3 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
             Classes
         </h2>
-        <div className='sm:col-start-1 row-start-2 mx-3 mb-2 md:flex justify-evenly gap-x-2'>
-          <div className='shadow-2xl hover:shadow-black hover:cursor-pointer sm:col-start-1 row-start-2 bg-gray-700 rounded-xl max-w-md:'>
+        <div className='sm:col-start-1 row-start-2 mx-3 md:flex justify-evenly gap-x-2'>
+          <div className='shadow-2xl hover:shadow-black hover:cursor-pointer sm:col-start-1 row-start-2 mb-2 bg-gray-700 rounded-xl max-w-md:'>
             <img src='/images/aerobics.jpg' className='p-2'/>
             <h3 className='text-white text-2xl text-center font-bold my-2 px-3'>
               Aerobics
@@ -16,7 +16,7 @@ const Classes = () => {
               Our aerobics classes allows our members to exercise and stretch in a relaxed and peaceful manner.
             </p>
           </div>
-          <div className='shadow-2xl hover:shadow-black hover:cursor-pointer sm:col-start-1 row-start-2 bg-gray-700 rounded-xl'>
+          <div className='shadow-2xl hover:shadow-black hover:cursor-pointer sm:col-start-1 row-start-2 mb-2 bg-gray-700 rounded-xl'>
             <img src='/images/boxing.jpg' className='p-2'/>
             <h3 className='text-white text-2xl text-center  font-bold my-2 px-3'>
               Boxing
@@ -26,7 +26,7 @@ const Classes = () => {
               or engage in some light sparing with with of our trained instructors.
             </p>
           </div>
-          <div className='shadow-2xl hover:shadow-black hover:cursor-pointer sm:col-start-1 row-start-2 bg-gray-700 rounded-xl px-2'>
+          <div className='shadow-2xl hover:shadow-black hover:cursor-pointer sm:col-start-1 row-start-2 mb-2 bg-gray-700 rounded-xl px-2'>
             <img src='/images/karate.jpg' className='p-2'/>
             <h3 className='text-white text-2xl text-center font-bold my-2 px-3'>
               Karate
