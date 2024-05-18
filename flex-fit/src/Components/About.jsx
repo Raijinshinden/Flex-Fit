9886eab887a878673461d7359f14f9df8e07185c
Ryan Dempsey <rjd9896@gmail.com>
2024-05-18 23:48:33 +0900

    Update About.jsx

--- flex-fit/src/Components/About.jsx
+++ flex-fit/src/Components/About.jsx
@@ -52,7 +52,7 @@ const About = () => {
       <div className='pt-3'>
         <img src='' className=''/>
         <p className='items-start bg-gray-700 rounded-lg p-3 block text-xl text-white'>
-            Rock climbing wall
+            Olym
         </p>
       </div>
     </div>
