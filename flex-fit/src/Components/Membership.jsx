9047006f1eaa9083e44fc3012e7419caad824ee7
Ryan Dempsey <rjd9896@gmail.com>
2024-06-14 23:55:49 +0900

    Update Membership.jsx

--- flex-fit/src/Components/Membership.jsx
+++ flex-fit/src/Components/Membership.jsx
@@ -31,7 +31,7 @@ const Membership = () => {
                   $50.00
                 </div>
             </div>
-            <div className='h-full sm:w-3/4 md:w-4/12 bg-gray-700 rounded-xl px-4 shadow-2xl hover:shadow-black hover:cursor-pointer sm:col-start-1 row-start-3 mx-3'>
+            <div className='h-full sm:w-3/4 sm:ml-16 md:w-4/12 bg-gray-700 rounded-xl px-4 shadow-2xl hover:shadow-black hover:cursor-pointer sm:col-start-1 row-start-3 mx-3'>
                 <h3 className='text-white text-2xl text-center font-bold my-2'>
                   Enthusiast
                 </h3>
