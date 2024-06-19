287d44f491ad03a52c7ce5307327147aff152c8f
Ryan Dempsey <rjd9896@gmail.com>
2024-06-19 23:58:05 +0900

    Update About.jsx

--- flex-fit/src/Components/About.jsx
+++ flex-fit/src/Components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <>
     <div className='grid-cols-2 grid-rows-7 w-full md:px-10 py-10 bg-gray-800 overflow-hidden' id='about'>
       <div className='rounded-full bg-gray-700'>
-      <h2 className='sm:col-start-1 row-auto mb-6 mx-3 md:mb-2 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
+      <h2 className='sm:col-start-1 row-start-1 mb-6 mx-3 md:mb-3 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
             About Us
         </h2>
       </div>
