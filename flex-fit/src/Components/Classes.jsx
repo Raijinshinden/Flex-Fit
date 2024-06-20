aac19ebeb88eba1dd7e8a348aef5176f1e7b513b
Ryan Dempsey <rjd9896@gmail.com>
2024-06-20 23:45:30 +0900

    Update Classes.jsx

--- flex-fit/src/Components/Classes.jsx
+++ flex-fit/src/Components/Classes.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Classes = () => { 
   return (
     <div className='grid w-full md:px-10 py-10 bg-gray-800 overflow-hidden' id='classes'>
-        <h2 className='sm:col-start- row-start-1 mb-6 mx-3 md:mb-3 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
+        <h2 className='sm:col-start-1 row-start-1 mb-6 mx-3 md:mb-3 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
             Classes
         </h2>
         <div className='sm:col-start-1 row-start-2 mx-12 md:flex justify-evenly gap-x-2'>
