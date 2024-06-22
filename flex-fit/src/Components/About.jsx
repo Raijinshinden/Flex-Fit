b5de7ec96beeddca84b32aefd799df5bd6fcf1ad
Ryan Dempsey <rjd9896@gmail.com>
2024-06-22 23:58:35 +0900

    Update About.jsx

--- flex-fit/src/Components/About.jsx
+++ flex-fit/src/Components/About.jsx
@@ -57,7 +57,7 @@ const About = () => {
           <div className='hover:cursor-pointer sm:col-start-1 row-start-2 m-4 bg-gray-700 rounded-xl md:w-3/12 md:h-full'>
             <img src='/images/Olympic-pool.jpg' className='rounded-lg float-left'/>
             <p className='items-start col-start-2 row-start-7 bg-gray-700 rounded-lg p-3 block text-xl text-white'>
-                Olympic Swimming Pool
+                
             </p>
           </div>
         </div>
