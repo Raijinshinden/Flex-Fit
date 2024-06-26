078b1b3e3d991e372ebde57592a4980e459086d5
Ryan Dempsey <rjd9896@gmail.com>
2024-06-26 23:54:06 +0900

    Update About.jsx

--- flex-fit/src/Components/About.jsx
+++ flex-fit/src/Components/About.jsx
@@ -15,11 +15,14 @@ const About = () => {
           <img src='/images/man-weights.jpg' className='size-96 sm:w-full md:float-right rounded-lg h-full'/>
         </div>
         <div className='col-start-3 col-span-2'>
-          <p className=' bg-gray-700 rounded-lg p-3 text-xl text-white'>
+          <p className='bg-gray-700 rounded-lg p-3 text-xl text-white'>
             Flex-Fit was founded with the idea that people should be able to exercise whenever they want to.<br/> 
             Because Flex-Fit is a 24/7 gym, our members are able to come exercise whenever they have free time.<br/> 
             At Flex-Fit, our overall goal is to instill in our members self-confidence, 
           </p>
+        </div>
+        <div className='row-start-2'>
+          <img src=''/>
         </div>
       </div>
     </div>
