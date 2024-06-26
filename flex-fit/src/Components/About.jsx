774c3500a7aea5dbd8916bffd4b77e598f61255f
Ryan Dempsey <rjd9896@gmail.com>
2024-06-26 23:43:24 +0900

    Update About.jsx

--- flex-fit/src/Components/About.jsx
+++ flex-fit/src/Components/About.jsx
@@ -12,53 +12,8 @@ const About = () => {
 
       <div className='sm:col-start-1 row-start-2 mx-12  lg:grid grid-cols-4 grid-rows-7 gap-10'>
         <div className='col-span-2 row-span-1'>
-        <img src='/images/man-weights.jpg' className='size-96 sm:w-full md:float-right rounded-lg h-full object-cover'/>
-        </div>
-        <div className='hover:cursor-pointer  bg-gray-700 rounded-xl md:w-3/12 md:h-full'>
-          <p className='items-start bg-gray-700 rounded-lg p-3 block text-xl text-white text-wrap'>
-              Flex-Fit was founded with the idea that people should be able to exercise whenever they want to.<br/> 
-              Because Flex-Fit is a 24/7 gym, our members are able to come exercise whenever they have free time.<br/> 
-              At Flex-Fit, our overall goal is to instill in our members self-confidence, 
-          </p>
-        </div>
-        <div className='hover:cursor-pointer bg-gray-700 rounded-xl md:w-3/12 md:h-full'>
-          <img src='' className=''/>
-          <p className='items-start bg-gray-700 rounded-lg p-3 block text-xl text-white'>
-              At Flex-Fit, you will find only the most state-of-the-art equipment that is readily available to the public.
-              Our Gyms are also <span className='text-amber-500 font-bold'>CAMERA-FREE</span> environments, so our members never have to worry about videos of themselves being
-              uploaded to the internet without their permission.
-              Flex-Fit also has a number of facilities besides our gym that are available to our higher level members.   
-          </p>
-        </div>
-        <div className='hover:cursor-pointer bg-gray-700 rounded-xl md:w-3/12 md:h-full'>
-          <img src='' className=''/>
-          <p className='items-start bg-gray-700 rounded-lg p-3 block text-xl text-white'>
-              These facilities include...
-          </p>
-        </div>
-        <div className='hover:cursor-pointer bg-gray-700 rounded-xl md:w-3/12 md:h-full'>
-          <img src='/images/Spa.jpg' className='rounded-lg float-left'/>
-          <p className='items-start bg-gray-700 rounded-lg p-3 block text-xl text-white'>
-              Luxury Spa
-          </p>
-        </div>
-        <div className='hover:cursor-pointer bg-gray-700 rounded-xl md:w-3/12 md:h-full'>
-          <img src='/images/Gym.jpg' className='rounded-lg float-right'/>
-          <p className='items-start bg-gray-700 rounded-lg p-3 block text-xl text-white'>
-              Gymnasium
-          </p>
-        </div>
-        <div className='hover:cursor-pointer bg-gray-700 rounded-xl md:w-3/12 md:h-full'>
-          <img src='' className=''/>
-          <p className='items-start bg-gray-700 rounded-lg p-3 block text-xl text-white'>
-              Rock climbing Wall
-          </p>
-        </div>
-        <div className='hover:cursor-pointer bg-gray-700 rounded-xl md:w-3/12 md:h-full'>
-          <img src='/images/Olympic-pool.jpg' className='rounded-lg float-left'/>
-          <p className='items-start col-start-2 row-start-7 bg-gray-700 rounded-lg p-3 block text-xl text-white'>
-              Olympic Swimming Pool
-          </p>
+          <img src='/images/man-weights.jpg' className='size-96 sm:w-full md:float-right rounded-lg h-full'/>
+
         </div>
       </div>
     </div>
