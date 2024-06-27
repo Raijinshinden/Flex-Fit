5f69a78c2b2c9970ad43bbbd7eef4a81abb8ee30
Ryan Dempsey <rjd9896@gmail.com>
2024-06-27 23:52:30 +0900

    Update About.jsx

--- flex-fit/src/Components/About.jsx
+++ flex-fit/src/Components/About.jsx
@@ -52,7 +52,7 @@ const About = () => {
 
           </p>
         </div>
-        <div className='row-start-5 row-span-1'>
+        <div className='row-start-5 row-span-1 col-start-3'>
           <img src='' />
         </div>
       </div>
