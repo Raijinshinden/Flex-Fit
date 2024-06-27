dc3fcf66957ee034e0fd44d375b038e3f812b19e
Ryan Dempsey <rjd9896@gmail.com>
2024-06-27 23:48:10 +0900

    Update About.jsx

--- flex-fit/src/Components/About.jsx
+++ flex-fit/src/Components/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
 
           </p>
         </div>
-        <div>
+        <div className='row-start-4 row-span-1 col-start-3 col-span-2 bg-gray-700 rounded-lg p-3 text-xl'>
         <h2 className='underline font-bold text-amber-500'>Gynasium</h2>
           <p className='text-white'>
 
