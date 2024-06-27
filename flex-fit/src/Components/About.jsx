98460db8ccc1d1d80b9950713aa84bbd0fe3607c
Ryan Dempsey <rjd9896@gmail.com>
2024-06-28 00:00:51 +0900

    Update About.jsx

--- flex-fit/src/Components/About.jsx
+++ flex-fit/src/Components/About.jsx
@@ -64,7 +64,7 @@ const About = () => {
 
           </p>
         </div>
-        <div className='row-start-7 row-span-1'>
+        <div className='row-start-7 row-span-1 col-start-1 col-span-2'>
           <h2 className='underline font-bold text-amber-500'>Rock Climbing Center</h2>
             <p className='text-white'>
 
