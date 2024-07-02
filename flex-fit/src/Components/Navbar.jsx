f17355ba59280ff110facd92ef7e93c1688d04c2
Ryan Dempsey <rjd9896@gmail.com>
2024-07-02 23:29:08 +0900

    Update Navbar.jsx

--- flex-fit/src/Components/Navbar.jsx
+++ flex-fit/src/Components/Navbar.jsx
@@ -2,11 +2,11 @@ const Navbar = () => {
 
   return (
     <header className='invisible md:visible w-full z-[99] bg-gray-600 h-15 fixed'>
-        <nav className='flex justify-between items-center w-[92%] mx-auto py-2'>
-            <div className=' text-5xl font-bold text-amber-500 hover:cursor-pointer'>
+        <nav className='flex lg:justify-between items-center w-[92%] mx-auto py-2'>
+            <div className='text-5xl font-bold text-amber-500 hover:cursor-pointer'>
                 Flex-Fit
             </div>
-            <div className='md:static absolute bg-gray-600 md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5'>
+            <div className='md:static absolute bg-gray-600 md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-3'>
                 <ul className='flex md:flex-row flex-col md:items-center md:gap-[7vw] gap-8'>
                     <li>
                         <a className='border-rounded text-xl p-2 text-amber-500 hover:bg-white hover:text-amber-600 hover:border-solid hover:border-2 hover:rounded-xl hover:border-amber-500 transition delay-100' href='#home'>Home</a>
