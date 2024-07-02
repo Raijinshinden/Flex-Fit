9777f0cec046fbbe22963b962063a6f8742d878d
Ryan Dempsey <rjd9896@gmail.com>
2024-07-03 00:43:07 +0900

    Update About.jsx

--- flex-fit/src/Components/About.jsx
+++ flex-fit/src/Components/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
           </p>
         </div>
         <div className='row-start-2 row-span-1 col-start-1 col-span-2'>
-          <p className='items-start bg-gray-700 rounded-lg p-3 text-xl text-white'>
+          <p className='items-start bg-gray-700 rounded-lg p-3 text-xl text-white h-full'>
                 At Flex-Fit, you will find only the most state-of-the-art equipment that is readily available to the public.
                 Our Gyms are also <span className='text-amber-500 font-bold'>CAMERA-FREE</span> environments, so our members never have to worry about videos of themselves being
                 uploaded to the internet without their permission.
