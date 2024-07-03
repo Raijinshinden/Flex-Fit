44265d7b340add9d51f87f0afce81608a0bc5028
Ryan Dempsey <rjd9896@gmail.com>
2024-07-04 00:03:32 +0900

    Update Membership.jsx

--- flex-fit/src/Components/Membership.jsx
+++ flex-fit/src/Components/Membership.jsx
@@ -8,7 +8,7 @@ const Membership = () => {
         </h2>
         <div className='sm:justify-center md:flex justify-evenly gap-3'>
             <div className='h-full pb-1 sm:mb-6 sm:w-3/4 sm:ml-16 md:w-4/12 bg-gray-700 rounded-xl px-4 shadow-2xl hover:shadow-black hover:cursor-pointer transition delay-75 hover:ease-in-out sm:col-start-1 row-start-2'>
-              <img src='/images/gym-basic.jpg' className='rounded-xl mt-3'/>
+              <img src='/images/gym-basic.jpg' className='rounded-xl mt-3 h-'/>
                 <h3 className='text-white text-2xl text-center font-bold my-2'>
                   Basic
                 </h3>
