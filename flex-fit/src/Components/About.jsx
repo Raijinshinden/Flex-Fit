eee06d87733bbbe81d1a9e34c85423ca44f1c15d
Ryan Dempsey <rjd9896@gmail.com>
2024-07-06 00:08:42 +0900

    Update About.jsx

--- flex-fit/src/Components/About.jsx
+++ flex-fit/src/Components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <>
-    <div className='grid-cols-2 grid-rows-6 w-full md:px-10 py-10 bg-gray-800 overflow-hidden' id='about'>
+    <div className='grid-cols-2 grid-rows-7 w-full md:px-10 py-10 bg-gray-800 overflow-hidden' id='about'>
       <div className='rounded-full bg-gray-700 mx-3'>
       <h2 className='sm:col-start-1 row-auto  mx-3 md:mb-2 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
             About Us
