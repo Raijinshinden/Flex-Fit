9bd596d7682ee00d0c2bc7a2223c6d7cc9392959
Ryan Dempsey <rjd9896@gmail.com>
2024-07-06 00:14:53 +0900

    Update About.jsx

--- flex-fit/src/Components/About.jsx
+++ flex-fit/src/Components/About.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const About = () => {
   return (
     <>
-    <div className='grid-cols-2 grid-rows-6 w-full md:px-10 bg-gray-800 overflow-hidden' id='about'>
+    <div className='grid-cols-2 grid-rows-1 w-full md:px-10 bg-gray-800 overflow-hidden' id='about'>
       <div className='rounded-full bg-gray-700 mx-3 mt-10'>
       <h2 className='sm:col-start-1 row-auto  mx-3 md:mb-2 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
             About Us
         </h2>
       </div>
 
-        <div className='col-start-1 row-start-2 mx-12 space-y-6 lg:grid grid-cols-4 grid-rows-8 gap-20'>
+        <div className='col-start-1 row-start-2 mx-12 space-y-6 lg:grid grid-cols-4 grid-rows-6 gap-20'>
           <div className='md:col-span-2 row-span-1'>
             <img src='/images/man-weights.jpg' className='size-96 md:w-full rounded-lg h-full mt-6'/>
           </div>
