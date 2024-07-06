75274f42a593efca02336d608c97644b3fcc8b38
Ryan Dempsey <rjd9896@gmail.com>
2024-07-06 23:37:42 +0900

    Update Classes.jsx

--- flex-fit/src/Components/Classes.jsx
+++ flex-fit/src/Components/Classes.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Classes = () => { 
   return (
     <div className='grid w-full md:px-10 py-10 bg-gray-800 overflow-hidden' id='classes'>
-        <h2 className='sm:col-start-1 row-start-1 mb-6 mx-3 md:mb-3 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
+        <h2 className='sm:col-start-1 row-start-1 mb-6 mx-3 md:mb-3 md:mt-10 bg-gray-700 text-center rounded-full text-3xl font-bold text-amber-500 py-4'>
             Classes
         </h2>
-        <div className='sm:col-start-1 row-start-2 mx-12 md:flex justify-evenly gap-x-10 mt-10'>
+        <div className='sm:col-start-1 row-start-2 mx-12 md:flex justify-evenly gap-x-10 mt-20 mb-20'>
           <div className='shadow-2xl p-2 hover:shadow-black hover:cursor-pointer transition delay-100 hover:ease-in-out sm:col-start-1 row-start-2 mb-6 bg-gray-700 rounded-xl md:w-3/12 md:h-full'>
             <img src='/images/aerobics.jpg' className='p-2 rounded-xl'/>
             <h3 className='text-white text-2xl text-center font-bold my-2 px-3'>
